21d5ae974859b2969a524b4aa0d14c66e0101320
OmegaMadLab <marco.obinu@omegamadlab.it>
2021-10-21 20:43:32 +0200

    review workflow

--- EnvironmentPreparation/deploy.ps1
+++ EnvironmentPreparation/deploy.ps1
@@ -2,12 +2,30 @@
 $devRg = New-AzResourceGroup -Name 'BicepAndGitHub-DEV-RG' -location 'westeurope'
 $prodRg = New-AzResourceGroup -Name 'BicepAndGitHub-PROD-RG' -location 'westeurope'
 
-# Create a service principal and grant it access on the DEV rg
+# Create a role for validating ARM deployment
+$role = Get-AzRoleDefinition | select -First 1
+
+$role.Id = $null
+$role.Name = "Deployment validator"
+$role.Description = "Can validate an ARM deployment."
+$role.Actions.RemoveRange(0,$role.Actions.Count)
+$role.Actions.Add("Microsoft.Resources/deployments/validate/action")
+$role.NotActions.RemoveRange(0,$role.NotActions.Count)
+$role.AssignableScopes.Add("/subscriptions/$((Get-AzContext).Subscription.Id)")
+
+New-AzRoleDefinition -Role $role
+
+# Create a service principal and grant it contributor access on the DEV rg
 $azureContext = Get-AzContext
 $servicePrincipal = New-AzADServicePrincipal `
     -DisplayName "BicepAndGitHub-DEV" `
     -Role "Contributor" `
     -Scope $devRg.ResourceId
+
+# Assign also the Deployment validator role on PROD RG
+New-AzRoleAssignment -ApplicationId $servicePrincipal.ApplicationId `
+    -ResourceGroupName $prodRg.ResourceGroupName `
+    -RoleDefinitionName "Deployment validator"
 
 $output = @{
    clientId = $($servicePrincipal.ApplicationId)
